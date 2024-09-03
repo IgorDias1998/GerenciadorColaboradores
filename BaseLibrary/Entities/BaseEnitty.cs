f7b679b56232fc30a9306d77b19d6e2a8a519dce
IgorDias1998 <igordiass1998@gmail.com>
2024-09-02 23:13:44 -0300

    Adicionar arquivos de projeto.

---
+++ BaseLibrary/Entities/BaseEnitty.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace BaseLibrary.Entities
+{
+    public class BaseEnitty
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        [JsonIgnore]
+        public List<Employee>? Employees { get; set; }
+    }
+}
---
+++ ServerLibrary/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BaseLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ServerLibrary.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<GeneralDepartment> GenaralDepartments { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Town> Towns { get; set; }
+        public DbSet<AplicationUser> AplicationUsers { get; set; }
+
+    }
+
+}
